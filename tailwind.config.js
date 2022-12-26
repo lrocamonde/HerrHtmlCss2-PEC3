52e4e98a66c02e856ea6682081c0368a1ade20c2
lrocamonde <lrocamonde@uoc.edu>
2022-12-26 21:05:38 +0100

    Preparado footer and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    colors: {
+      backblue: '#658ce0',
+      white: '#ffffff',
+      black: '#000000',
+      gray: '#343a40'
+    },
     extend: {},
   },
   plugins: [],
