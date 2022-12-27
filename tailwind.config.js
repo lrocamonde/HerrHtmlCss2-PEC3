d67d0e7b7bc8dcdf1a536f6245a38c6bf74122f0
lrocamonde <lrocamonde@uoc.edu>
2022-12-27 20:36:51 +0100

    Index preparado

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,31 @@ module.exports = {
   theme: {
     colors: {
       backblue: '#658ce0',
-      white: '#ffffff',
-      black: '#000000',
-      gray: '#343a40'
+      white: '#f2f2f2',
+      black: '#212529',
+      gray: '#343a40',
+      red: 'red'
     },
-    extend: {},
+    fontFamily: {
+      'helvet': ["Segoe UI","Roboto","Helvetica Neue","Arial","sans-serif","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"]
+    },
+    minWidth: {
+      '480': '480px',
+    },
+    screens: {
+      'bk1': '883px',
+      'bk2': '1255px'
+    },
+    extend: {
+      gridTemplateColumns: {
+        'small': '3fr 2fr',
+        'medium': 'repeat(2,1fr) 2fr repeat(2,1fr)',
+        'large': '3fr 3fr 2fr 1fr'
+      },
+      gridTemplateRows: {
+        'medium': '2fr repeat(3,1fr)'
+      }
+    },
   },
   plugins: [],
 }
