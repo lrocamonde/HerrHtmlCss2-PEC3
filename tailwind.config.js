3c799646ea9bc51aeb6715c8cb4eb050cf18aa2f
lrocamonde <lrocamonde@uoc.edu>
2022-12-29 20:00:43 +0100

    Cambios finales

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,6 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    colors: {
-      backblue: '#658ce0',
-      white: '#f2f2f2',
-      black: '#212529',
-      gray: '#343a40',
-      red: 'red',
-      backcard: '#d3d3d34d'
-    },
-    fontFamily: {
-      'helvet': ["Segoe UI","Roboto","Helvetica Neue","Arial","sans-serif","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"]
-    },
-    minWidth: {
-      '480': '480px',
-    },
-    screens: {
-      'bk1': '883px',
-      'bk2': '1255px'
-    },
     extend: {
       gridTemplateColumns: {
         'small': '3fr 2fr',
@@ -28,7 +10,32 @@ module.exports = {
       },
       gridTemplateRows: {
         'medium': '2fr repeat(3,1fr)'
-      }
+      },
+      colors: {
+        backblue: '#658ce0',
+        white: '#f2f2f2',
+        black: '#212529',
+        gray: '#343a40',
+        red: 'red',
+        backcard: '#d3d3d34d'
+      },
+      fontFamily: {
+        'helvet': ["Segoe UI","Roboto","Helvetica Neue","Arial","sans-serif","Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"]
+      },
+      fontSize: {
+        'calc1': 'calc(10px + .8vw)',
+        'calc2': 'calc(10px + .4vw)',
+        'calc3': 'calc(1.375rem + 1.5vw)',
+        'calc4': 'calc(10px + 1.2vw)',
+        'calch1': 'calc(20px + 2.2vw)'
+      },
+      minWidth: {
+        '480': '480px',
+      },
+      screens: {
+        'bk1': '883px',
+        'bk2': '1255px'
+      },
     },
   },
   plugins: [],
